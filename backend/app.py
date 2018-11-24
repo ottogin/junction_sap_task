b32b82622c833f49a51d359f13b006ab95385c2c
Michael Vasilkovsky <smashmushrooms@gmail.com>
2018-11-24 13:15:17 +0200

    Add backend

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/getAnswer', methods=['POST'])
+def getAnswer():
+    data = request.json['text']
+    
+
+if __name__ == '__main__':
+    app.run()
---
+++ backend/faq.py
@@ -0,0 +1,7 @@
+import numpy as np
+import fasttext
+
+from nltk import WordPunctTokenizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from gensim.models import KeyedVectors
+
